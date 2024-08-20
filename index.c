d4ab0263399ecd2bb627b3e5d4b2d4a532f45598
NOUR-EDDINE MOUNCET <desnake.dev@gmail.com>
2024-08-20 04:01:48 +0100

    Update index.c

---
+++ index.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cptype.h>
+#include <string.h>
+#include <cs50.h>
+
+int main (void){
+  char *s = get_string("S: "):
+  char *t = malloc(strlen(s) + 1 );
+
+  for(int i = 0; i < strlen(s) + 1; i++)
+    {
+      t[i] = s[i];
+    }
+  t[0] = toupper(t[0]);
+  printf("S: %s\n,s);
+  printf("T: %s\n,t);
+  return 0;
+  }
